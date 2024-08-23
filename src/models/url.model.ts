124c34a7849f9b63d2147afc6f712ab35b28c0e2
miracleonyenma <miracleficient@gmail.com>
2024-08-23 15:19:18 +0100

    Update URLSchema to include timestamps for createdAt and updatedAt

--- src/models/url.model.ts
+++ src/models/url.model.ts
@@ -1,37 +1,38 @@
 import mongoose, { Schema } from "mongoose";
 
-const URLSchema = new Schema({
-  url: {
-    type: String,
-    required: true,
+const URLSchema = new Schema(
+  {
+    url: {
+      type: String,
+      required: true,
+    },
+    shortUrl: {
+      type: String,
+      required: true,
+    },
+    code: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    image: {
+      type: String,
+      required: false,
+      default:
+        "https://res.cloudinary.com/alphas/image/upload/v1723571495/clnk/clnk-icon_2x_qdqhjl.png",
+    },
+    user: {
+      type: Schema.Types.ObjectId,
+      ref: "User",
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+    },
   },
-  shortUrl: {
-    type: String,
-    required: true,
-  },
-  code: {
-    type: String,
-    required: true,
-    unique: true,
-  },
-  image: {
-    type: String,
-    required: false,
-    default:
-      "https://res.cloudinary.com/alphas/image/upload/v1723571495/clnk/clnk-icon_2x_qdqhjl.png",
-  },
-  user: {
-    type: Schema.Types.ObjectId,
-    ref: "User",
-  },
-  createdAt: {
-    type: Date,
-    default: Date.now,
-  },
-  updatedAt: {
-    type: Date,
-    default: Date.now,
-  },
-});
+  {
+    timestamps: true,
+  }
+);
 
 export default mongoose.model("URL", URLSchema);
